f62e291c03bcb76982d823fe0a8ce34c5397c1ff
jiasu <45795504+Haiezan@users.noreply.github.com>
2023-06-14 14:53:28 +0800

    01fwrite2

---
+++ 01fwrite/01fwrite2/01fwrite2.cpp
@@ -0,0 +1,24 @@
+﻿// 01fwrite2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+int main()
+{
+    // 使用 "wb" 二进制写的方式打开文件
+    FILE* p = fopen("D:\\a.txt", "wb");
+
+    // 设置写入变量
+    int number = 0x12345678; //0x 16进制
+
+    // buffer : 将指针对应的内存的写出到文件中
+    // sizeof(char) : 写出的基本单元字节长度
+    // sizeof(buffer) : 写出的基本单元个数,
+    //       写出字节个数是 sizeof(buffer) * sizeof(char)
+    // p : 文件指针
+    // 返回值 : fwrite 函数返回值表示写出到的 基本单元 的个数
+    size_t count = fwrite(&number, sizeof(int), 1, p);
+
+    printf("Main End\n");
+    return 0;
+}
